4bedf6a4616613b000a1bf711adadcc3ef7ff43b
godgodgod11101 <a008502506@gmail.com>
2020-08-20 11:53:04 +0800

    20200820 class
    private name

---
+++ mymodule.py
@@ -0,0 +1,9 @@
+"""mymath - our example math module"""
+_pi = 3.14159
+def area(r):
+    """area(r): return the area of a circle with radius r."""
+    global _pi
+    return(_pi * r * r)
+
+# single-comment
+"""multi-comment"""
